d5f51bc7e8fb80898f322bf91a2d058042e6825b
Saran-R13 <saranyogi13022002@gmail.com>
2026-02-05 23:33:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return <Home />;
+}
+function Home() {
+  return (
+    <div className="first-container">
+      <p className="fstline">DreamHomes&Land</p>
+      <ul class="tags">
+        <li> Home</li>
+        <li>Categories</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
