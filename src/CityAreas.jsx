6d403de75772edf67becd7b8b3c6330284d5743b
Saran-R13 <saranyogi13022002@gmail.com>
2026-02-19 18:31:47 +0530

    .

--- src/CityAreas.jsx
+++ src/CityAreas.jsx
@@ -1,21 +1,3 @@
-// export function CityAreas() {
-//   return (
-//     <section>
-//       <ul>
-//         <li><a href="#">Chennai</a></li>
-//         <li><a href="#">Coimbatore</a></li>
-//         <li><a href="#">Madurai</a></li>
-//         <li><a href="#">Tiruchirappalli</a></li>
-//         <li><a href="#">Salem</a></li>
-//         <li><a href="#">Tirunelveli</a></li>
-//         <li><a href="#">Erode</a></li>
-//         <li><a href="#">Vellore</a></li>
-//         <li><a href="#">Thoothukudi</a></li>
-//         <li><a href="#">Thanjavur</a></li>
-//       </ul>
-//     </section>
-//   );
-
 import { useNavigate } from "react-router-dom";
 
 // }
@@ -25,7 +7,6 @@ export function CityAreas() {
     <section className="city-properties">
       <div className="city-fst-container">
         <h2>Strategic Locations Across Tamil Nadu</h2>
-
         <p>
           Unlock premium land assets in rapidly developing urban hubs with
           long-term value and growth potential.
@@ -36,39 +17,6 @@ export function CityAreas() {
         <button onClick={() => navigate("/coimbatorePage")}>Coimbatore</button>
         <button onClick={() => navigate("/maduraiPage")}>Madurai</button>
         <button onClick={() => navigate("/trichyPage")}>Trichy</button>
-
-        {/* <ul>
-          <li>
-            <a href="#">Chennai</a>
-          </li>
-          <li>
-            <a href="#">Coimbatore</a>
-          </li>
-          <li>
-            <a href="#">Madurai</a>
-          </li>
-          <li>
-            <a href="#">Tiruchirappalli</a>
-          </li>
-          <li>
-            <a href="#">Salem</a>
-          </li>
-          <li>
-            <a href="#">Tirunelveli</a>
-          </li>
-          <li>
-            <a href="#">Erode</a>
-          </li>
-          <li>
-            <a href="#">Vellore</a>
-          </li>
-          <li>
-            <a href="#">Thoothukudi</a>
-          </li>
-          <li>
-            <a href="#">Thanjavur</a>
-          </li>
-        </ul> */}
       </div>
       <button onClick={() => navigate(-1)}>Back</button>
     </section>
